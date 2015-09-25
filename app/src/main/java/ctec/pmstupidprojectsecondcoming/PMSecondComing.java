1f5a012122e3e9bf8ba385f1d64170183d40c2e2
bleepbloop13 <rbak9218@740s-pl-107.local>
2015-09-25 13:10:28 -0600

    Added color change

--- app/src/main/java/ctec/pmstupidprojectsecondcoming/PMSecondComing.java
+++ app/src/main/java/ctec/pmstupidprojectsecondcoming/PMSecondComing.java
@@ -1,37 +1,69 @@
 package ctec.pmstupidprojectsecondcoming;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.view.View;
+import android.graphics.Color;
 
-public class PMSecondComing extends AppCompatActivity {
+public class PMSecondComing extends AppCompatActivity
+{
+
+    private Button colorChangeButton;
+    private RelativeLayout background;
+    private TextView sillyWords;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pmsecond_coming);
+
+        colorChangeButton = (Button) findViewById(R.id.colorButton);
+        background = (RelativeLayout) findViewById(R.id.appBackground);
+        sillyWords = (TextView) findViewById(R.id.sillyWords);
+
+        setupListeners();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_pmsecond_coming, menu);
-        return true;
+
+
+
+
+    private void changeColors()
+    {
+        int redColor;
+        int blueColor;
+        int greenColor;
+
+        redColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+
+        background.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+        redColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+
+        colorChangeButton.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void setupListeners()
+    {
+        colorChangeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View buttonView)
+            {
+                //This is where you put code that happens when you click a button
+                changeColors();
+            }
+        });
     }
 }
+
